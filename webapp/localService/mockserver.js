f80844334cdaec648d9163857eae9a00496b997b
Liang Liang Wang <liang.liang.wang@sap.com>
2017-02-16 02:18:03 +0000

    Calling a Function Import By using a function import that calculates these items on the back end we do not need to do the calculation on the client. The mock server will be instructed to do the calculation locally for testing purposes.

--- webapp/localService/mockserver.js
+++ webapp/localService/mockserver.js
@@ -19,7 +19,35 @@ sap.ui.define([
 				sMockdataBaseUrl: "../localService/mockdata",
 				bGenerateMissingMockData: true
 			});
-			
+
+			/**
+			 * We push a new request handler to mock the function import call as follows:
+				1. Fetch the array of requests from the MockServer. 
+				 The mock server holds an internal list of requests that you have to get and set if you want to modify.
+				2. Push a new request handler to handle the function import
+				3. Set the updated request array
+			 * 
+			 */
+			var aRequests = oMockServer.getRequests();
+			aRequests.push({
+				method: "GET",
+				path: new RegExp("FindUpcomingMeetups(.*)"),
+				response: function(oXhr, sUrlParams) {
+					jQuery.sap.log.debug("Incoming request for FindUpcomingMeetups");
+					var today = new Date(2016, 9, 30);
+					today.setHours(0); // or today.toUTCString(0) due to timezone differences
+					today.setMinutes(0);
+					today.setSeconds(0);
+					//console.log(today.getTime());
+					var oResponse = jQuery.sap.sjax({
+						url: "/Meetups?$filter=EventDate ge " + "/Date(" + today.getTime() + ")/"
+					});
+					oXhr.respondJSON(200, {}, JSON.stringify(oResponse.data));
+					return true;
+				}
+			});
+			oMockServer.setRequests(aRequests);
+
 			var fnCustom = function(oEvent) {
 				var oXhr = oEvent.getParameter("oXhr");
 				if (oXhr && oXhr.url.indexOf("first") > -1) {
